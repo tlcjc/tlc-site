931a92df3163a973809891755156c9bc9896b55d
tlcjc <menekouc@gmail.com>
2018-07-19 18:57:23 -0400

    commit fix

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+}
